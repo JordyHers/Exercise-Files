15efd3cfd5fd0566269ef044b4228e5d60524df4
jordyhers <jordy925@gmail.com>
2022-03-17 22:37:02 +0300

    WebScraping update

--- Ch8/scrapePages.py
+++ Ch8/scrapePages.py
@@ -4,12 +4,11 @@ import requests
 url = 'https://scrapingclub.com/exercise/list_basic/?page=1'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
-
 items = soup.find_all('div', class_="col-lg-4 col-md-6  mb-4")
 
 count = 1
 for i in items:
-    itemName = i.find('h4', class_='card-title').text
+    itemName = i.find('h4', class_='card-title').text.strip('')
     itemPrice = i.find('h5').text
     print('%s ) Price: %s, Item Name: %s' % (count, itemPrice, itemName))
     count = count + 1
