4da5adc171438019e9199af20bdec636026f75c6
jordyhers <jordy925@gmail.com>
2022-04-04 21:07:12 +0300

    Web scrapin JordyHers

---
+++ Ch8/scrape_jordyhers.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'http://jordyhers.com'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'lxml')
+# print(soup)
+
+# Here we request all the meta tags from the website
+meta = soup.find_all('meta')
+
+
+for each in meta:
+    print(each)
